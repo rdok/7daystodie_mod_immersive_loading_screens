6cec664e3a203a33d923c8238c360a2855121391
Rizart Dokollari <r.dokollari@gmail.com>
2024-08-16 00:06:28 +0100

    chore: Setup code injection.

---
+++ ImmersiveLoadingScreens/Harmony/Main.cs
@@ -0,0 +1,17 @@
+﻿namespace ImmersiveLoadingScreens.Harmony
+{
+    public class Main
+    {
+        private static readonly ILogger Logger = new Logger();
+
+        public class Init : IModApi
+        {
+            public void InitMod(Mod modInstance)
+            {
+                const string id = "uk.co.rdok.7daystodie.mods.immersive_loading_screens";
+                var harmony = new HarmonyLib.Harmony(id);
+                harmony.PatchAll();
+            }
+        }
+    }
+}
